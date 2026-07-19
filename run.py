832d88b973961cb29d1172f5eb6b8c7063245924
Chris G. <121430900+spliter90@users.noreply.github.com>
2026-07-19 11:10:49 +0200

    Register network settings routes

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from waitress import serve
 from pistreamer.app import app, config
 import pistreamer.settings_pages  # noqa: F401
+import pistreamer.network  # noqa: F401
 
 if __name__ == "__main__":
     serve(app, host=config["web"]["host"], port=int(config["web"]["port"]), threads=4)
