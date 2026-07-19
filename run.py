08ff49a43ca404afbb12b14f00ef2e7f43141581
Chris G. <121430900+spliter90@users.noreply.github.com>
2026-07-19 21:35:24 +0200

    Register diagnostics and persistent server logging

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from pistreamer.app import app, config
 import pistreamer.settings_pages  # noqa: F401
 import pistreamer.network  # noqa: F401
 import pistreamer.updater  # noqa: F401
+import pistreamer.diagnostics  # noqa: F401
 
 if __name__ == "__main__":
     serve(app, host=config["web"]["host"], port=int(config["web"]["port"]), threads=4)
