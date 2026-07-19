15a102703ad9a7f891c9bc7a1d2f62efdd0f318a
Chris G. <121430900+spliter90@users.noreply.github.com>
2026-07-19 11:01:38 +0200

    Register settings submenus

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from waitress import serve
 from pistreamer.app import app, config
+import pistreamer.settings_pages  # noqa: F401
 
 if __name__ == "__main__":
     serve(app, host=config["web"]["host"], port=int(config["web"]["port"]), threads=4)
